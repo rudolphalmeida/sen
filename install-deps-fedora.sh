c6ea938fecef24ed875de014364ce3838439d7f8
Rudolph Almeida <rudolf1.almeida@gmail.com>
2026-03-10 17:06:06 +1100

    Fedora deps

--- install-deps-fedora.sh
+++ install-deps-fedora.sh
@@ -2,7 +2,12 @@ set -x
 
 sudo dnf install autoconf autoconf-archive automake libtool perl-open perl-FindBin
 
-# OpenGL and windowing reqs.
-sudo dnf install libXcursor-devel libXi-devel libXinerama-devel libXrandr-devel
-sudo dnf install wayland-devel libxkbcommon-devel wayland-protocols-devel extra-cmake-modules
-sudo dnf install mesa-libGL-devel
+# SDL3 deps.
+sudo dnf install gcc git-core make cmake \
+alsa-lib-devel fribidi-devel pulseaudio-libs-devel pipewire-devel \
+libX11-devel libXext-devel libXrandr-devel libXcursor-devel libXfixes-devel \
+libXi-devel libXScrnSaver-devel libXtst-devel dbus-devel ibus-devel \
+systemd-devel mesa-libGL-devel libxkbcommon-devel mesa-libGLES-devel \
+mesa-libEGL-devel vulkan-devel wayland-devel wayland-protocols-devel \
+libdrm-devel mesa-libgbm-devel libusb1-devel libdecor-devel \
+pipewire-jack-audio-connection-kit-devel libthai-devel zlib-ng-compat-static
